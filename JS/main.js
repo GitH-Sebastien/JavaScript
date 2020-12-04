8989650dd789b177227ea69a46173fae59102d51
Sebastien <gith.sebastien@gmail.com>
2020-12-04 02:48:39 +0100

    Define the program

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+/*
+Je vérifie si une valeur existe
+Je cree un tableau qui contiendra les valeurs rentré 
+Je créer une variable qui est égal a la valeur rentré 
+Je pousse cette variable dans un tableau tableau
+Je crée un nombre aléatoire qui aura la taille maximale du tableau
+J'affiche la valeur choisi aléatoirement
+*/
