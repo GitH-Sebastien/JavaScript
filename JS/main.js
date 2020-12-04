8f9860d88c3f8f546a798eebd9fa3b85428c33fd
Sebastien <gith.sebastien@gmail.com>
2020-12-04 03:31:53 +0100

    Add button and get value from the input

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,15 @@
 /*
-Je vérifie si une valeur existe
 Je cree un tableau qui contiendra les valeurs rentré 
 Je créer une variable qui est égal a la valeur rentré 
 Je pousse cette variable dans un tableau tableau
 Je crée un nombre aléatoire qui aura la taille maximale du tableau
 J'affiche la valeur choisi aléatoirement
 */
+
+let listOfViewers = [];
+
+let getName = () => {
+    let name = document.getElementById('name').value;
+    listOfViewers.push(name);
+    console.log(listOfViewers);
+}
